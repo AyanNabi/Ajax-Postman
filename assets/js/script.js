dfe0b079e53eca57bf124ace7b62152cb62f2299
AyanNabi <ayanmna@code.edu.az>
2023-03-23 23:56:44 +0400

    feat:Mini app for month is created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,87 @@
-
-
-
-
 $(document).ready(function () {
-    let id = $("input").val();
+
     $("form").submit(function (e) {
         e.preventDefault();
 
-        $.ajax({
-            url: `https://api.aladhan.com/v1/calendar/2017/4?latitude=40.4093&longitude=49.8671&method=${id}`,
-            method: "get",
-            success: function (data) {
-                console.log("birincde");
-                console.log(data);
-              
-            }
+        let longtitude = $("#longtitude").val();
+        let latitude = $("#latitude").val();
 
+        var date1 = new Date($('#date').val());
+        let day = date1.getDate();
+        let month = date1.getMonth() + 1;
+        let year = date1.getFullYear();
 
-
-        })
-    })
-
-    let id2 = $("#input").val();
-    $("form").submit(function (e) {
-        e.preventDefault();
 
         $.ajax({
-            url: `http://api.aladhan.com/v1/calendar/2017/4?latitude=51.508515&longitude=-0.1254872&method=${id2}`,
+            url: `http://api.aladhan.com/v1/calendar/${year}/${month}?latitude=51.508515&longitude=-0.1254872&method=2`,
             method: "get",
-            success: function (data) {
-                console.log("yeni");
-                data.data.forEach(element => {
-                    let array = Object.entries(element.timings)
-                    console.log(array);
+            success: function (whole) {
+                $("table").append(`
+                        
+                <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Asr</th>
+                  <th scope="col">Dhuhr</th>
+                  <th scope="col">Fajr</th>
+                  <th scope="col">Firstthird</th>
+                  <th scope="col">Imsak</th>
+                  <th scope="col">Isha</th>
+                  <th scope="col">Lastthird</th>
+                  <th scope="col">Maghrib</th>
+                  <th scope="col">Midnight</th>
+                  <th scope="col">Sunrise</th>
+                  <th scope="col">Sunset</th>
+                </tr>
+              </thead>
+                
+                
+                
+                
+                `)
+
+                whole.data.forEach(element => {
+                    let dataDay = ((element.date.gregorian.date).split("-"))[0];
+                    let dataMonth = ((element.date.gregorian.date).split("-"))[1];
+                    let dataYear = ((element.date.gregorian.date).split("-"))[2];
+
+                    // aylara fore isleyir
+                    if (dataMonth == month && dataYear == year) {
+                        // $("table").append(`
+                        // <tbody>
+                        //     <tr>
+                        //     <th scope="row">1</th>
+                        //     <td>${element.timing.Asr}</td>
+                        //     <td>${element.timing.Dhuhr}</td>
+                        //     <td>${element.timing.Fajr}</td>
+                        //     <td>${element.timing.Firstthird}</td>
+                        //     <td>${element.timing.Imsak}</td>
+                        //     <td>${element.timing.Isha}</td>
+                        //     <td>${element.timing.Lastthird}</td>
+                        //     <td>${element.timing.Maghrib}</td>
+                        //     <td>${element.timing.Midnight}</td>
+                        //     <td>${element.timing.Sunrise}</td>
+                        //     <td>${element.timing.Sunset}</td>
+                        //     </tr>
+                            
+                        // </tbody>
+                                                
+                        // `)
+                        console.log(element.timings);
+                    }
+                    //gune gore ishleyir
+                    // if (dataMonth == month && dataYear == year && dataDay == day) {
+                    //     console.log(element);
+                    // }
+
                 });
+
             }
+
         })
-
-
-
     })
+
+
 })
 
 
