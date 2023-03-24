a1a601db474f88eb67d1cd7710dc3612ee03acd0
AyanNabi <ayanmna@code.edu.az>
2023-03-25 00:57:00 +0400

    feat:task is recovered

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,88 +1,107 @@
 $(document).ready(function () {
-
     $("form").submit(function (e) {
+
         e.preventDefault();
-
-        let longtitude = $("#longtitude").val();
-        let latitude = $("#latitude").val();
-
-        var date1 = new Date($('#date').val());
-        let day = date1.getDate();
-        let month = date1.getMonth() + 1;
-        let year = date1.getFullYear();
+        $("tbody").html("");
 
 
-        $.ajax({
-            url: `http://api.aladhan.com/v1/calendar/${year}/${month}?latitude=51.508515&longitude=-0.1254872&method=2`,
-            method: "get",
-            success: function (whole) {
-                $("table").append(`
-                        
-                <thead>
-                <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Asr</th>
-                  <th scope="col">Dhuhr</th>
-                  <th scope="col">Fajr</th>
-                  <th scope="col">Firstthird</th>
-                  <th scope="col">Imsak</th>
-                  <th scope="col">Isha</th>
-                  <th scope="col">Lastthird</th>
-                  <th scope="col">Maghrib</th>
-                  <th scope="col">Midnight</th>
-                  <th scope="col">Sunrise</th>
-                  <th scope="col">Sunset</th>
-                </tr>
-              </thead>
-                
-                
-                
-                
-                `)
+        // let longtitude = GpsLocation
+        // let latitude = 
+        // var date1 = new Date($('#date').val());
+        // let day = date1.getDate();
+        // let month = date1.getMonth() + 1;
+        // let year = date1.getFullYear();
 
-                whole.data.forEach(element => {
-                    let dataDay = ((element.date.gregorian.date).split("-"))[0];
-                    let dataMonth = ((element.date.gregorian.date).split("-"))[1];
-                    let dataYear = ((element.date.gregorian.date).split("-"))[2];
+        let month2 = Number($('#month option:selected').val());
+        let year2 = Number($("#year2").val());
+        
+       
+            $.ajax({
+                url: `http://api.aladhan.com/v1/calendar/${year2}/${month2}?latitude=51.508515&longitude=-0.1254872&method=2`,
+                method: "get",
+                success: function (whole) {
+    
+                    $("section").removeClass("d-none");
+    
+                  id = 1;
+                    whole.data.forEach(element => {
+                        let dataDay = ((element.date.gregorian.date).split("-"))[0];
+                        let dataMonth = ((element.date.gregorian.date).split("-"))[1];
+                        let dataYear = ((element.date.gregorian.date).split("-"))[2];
+    
+    
+                        if (dataMonth == month2 && dataYear == year2) {
+    
+                            // $("tbody").append(`
+                            //     <tr>
+                            //     <th scope="row">${id}</th>
+                            //     <td>${element.timings.Fajr}</td>
+                            //     <td>${element.timings.Sunrise}</td>
+                            //     <td>${element.timings.Dhuhr}</td>
+                            //     <td>${element.timings.Asr}</td>
+                            //     <td>${element.timings.Sunset}</td>
+                            //     <td>${element.timings.Maghrib}</td>
+                            //     <td>${element.timings.Isha}</td>
+                            //     <td>${element.timings.Imsak}</td>
+                            //     <td>${element.timings.Midnight}</td>
+                            //     <td>${element.timings.Firstthird}</td>
+                            //     <td>${element.timings.Lastthird}</td>
+                            //     </tr>
+                            // `);
+                            $("#Fajr").text(element.timings.Fajr);
+                            $("#SUNRISE").text(element.timings.Sunrise);
+                            $("#DHUHR").text(element.timings.Dhuhr);
+                            $("#ASR").text(element.timings.Asr);
+                            $("#Sunset").text(element.timings.Sunset);
+                            $("#Maghrib").text(element.timings.Maghrib);
+                            $("#Isha").text(element.timings.Isha);
+                            $("#Imsak").text(element.timings.Imsak);
+                            $("#Midnight").text(element.timings.Midnight);
+                            $("#Firstthird").text(element.timings.Firstthird);
+                            $("#Lastthird").text(element.timings.Lastthird);
 
-                    // aylara fore isleyir
-                    if (dataMonth == month && dataYear == year) {
-                        // $("table").append(`
-                        // <tbody>
-                        //     <tr>
-                        //     <th scope="row">1</th>
-                        //     <td>${element.timing.Asr}</td>
-                        //     <td>${element.timing.Dhuhr}</td>
-                        //     <td>${element.timing.Fajr}</td>
-                        //     <td>${element.timing.Firstthird}</td>
-                        //     <td>${element.timing.Imsak}</td>
-                        //     <td>${element.timing.Isha}</td>
-                        //     <td>${element.timing.Lastthird}</td>
-                        //     <td>${element.timing.Maghrib}</td>
-                        //     <td>${element.timing.Midnight}</td>
-                        //     <td>${element.timing.Sunrise}</td>
-                        //     <td>${element.timing.Sunset}</td>
-                        //     </tr>
-                            
-                        // </tbody>
-                                                
-                        // `)
-                        console.log(element.timings);
-                    }
-                    //gune gore ishleyir
-                    // if (dataMonth == month && dataYear == year && dataDay == day) {
-                    //     console.log(element);
-                    // }
 
-                });
 
-            }
+
+                            // id += 1;
+                        }
+                        //gune gore ishleyir
+                        // if (dataMonth == month && dataYear == year && dataDay == day) {
+                        //     console.log(element);
+                    } // }
+    
+                    
+                    )
+                   
+    
+                   
+                }
+            })
+          
+
+
+
+
 
         })
+
+
+
+
+
+      
+       
+
+
     })
 
 
-})
+
+
+
+
+
+
 
 
 
